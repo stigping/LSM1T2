258da693b1c12d7672f682e6aa5c6d11f1e1ef9c
Stig Omdal <stigping@gmail.com>
2020-06-17 09:32:41 +0200

    Changes
    Last changes before studyhours.

--- liveshare/ls9/js/menu.js
+++
@@ -1,9 +0,0 @@
-
-    function openMenu() {
-        var x = document.getElementById("menuLinks");
-        if (x.style.display === "block") {
-            x.style.display = "none";
-        } else {
-            x.style.display = "block";
-        }
-    }
